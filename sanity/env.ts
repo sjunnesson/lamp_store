5c369b96864a6399aa4ce4fe6cafb41b3100f3e6
David Sjunnesson <davidsjunnesson@Mac.lan>
2026-01-18 01:45:33 +0100

    Fix dataset default value in sanity/env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,10 +1,7 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-01-17'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
